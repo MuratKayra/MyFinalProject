b3428b1909d6b8a5a7f9a8e2a8321733c6afd76e
MuratKayra <muratkayra93@gmail.com>
2024-07-21 20:48:05 +0300

    8. Gün ders sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,18 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 
 namespace ConsoleUI
 {
     internal class Program
     {
+        //SOLID
+        //Open Closed Principle
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAll())
+            foreach (var product in productManager.GetByUnitPrice(10, 20))
             {
                 Console.WriteLine("{0} {1} {2}", product.ProductId, product.ProductName, product.UnitPrice);
             }
